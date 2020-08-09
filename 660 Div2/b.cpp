1407aaf149fc4f09eeab036663ce5ee45e4311b8
Shivansh Gupta <69420564+sgigitww@users.noreply.github.com>
2020-08-09 18:29:44 +0530

    Update b.cpp

--- 660 Div2/b.cpp
+++ 660 Div2/b.cpp
@@ -5,20 +5,20 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-#define ff 					first
-#define ss 					second
+#define ff 				first
+#define ss 				second
 #define sz(x) 				(int)(x).size()
 #define all(x) 				begin(x), end(x)
 #define rall(x) 			(x).rbegin(), (x).rend() 
 #define rsz 				resize
 #define ins 				insert 
-#define ft 					front() 
-#define bk 					back()
-#define pf 					push_front 
-#define pb 					push_back
-#define eb 					emplace_back 
-#define lb 					lower_bound 
-#define ub 					upper_bound 
+#define ft 				front() 
+#define bk 				back()
+#define pf 				push_front 
+#define pb 				push_back
+#define eb 				emplace_back 
+#define lb 				lower_bound 
+#define ub 				upper_bound 
 #define pqmax				priority_queue<int>
 #define pqmin				priority_queue<int,vector<int>,greater<int>>
 #define transup(x)			transform(x.begin(),x.end(),x.begin(),::toupper)
@@ -27,7 +27,7 @@ typedef long double ld;
 #define per(i,a,b) 			for (int i = (b)-1; i >= (a); --i)
 #define trav(a,x) 			for (auto a: x)
 
-#define setbits(x)  		__builtin_popcountll(x)
+#define setbits(x)  		_	_builtin_popcountll(x)
 #define zerobits(x)			__builtin_ctzll(x)
 
 const ll mod = 1e9+7; // 998244353;
@@ -40,7 +40,7 @@ int dy[] = {0, 0, 1, -1};
 //WA
 void solve(){
 
-	int n;
+    int n;
     cin>>n;
     int eight = (n+3)/4;
     int nine = n-eight;
@@ -58,17 +58,17 @@ int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-    auto start = std::chrono::high_resolution_clock::now();
-    /*start from here*/
+   	auto start = std::chrono::high_resolution_clock::now();
+    	/*start from here*/
     
-    int t;
-    cin>>t;
-    while(t--){
-    	solve();
-    }
+	    int t;
+	    cin>>t;
+	    while(t--){
+		solve();
+	    }
 
 	auto stop = std::chrono::high_resolution_clock::now(); 
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start); 
+    	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start); 
     
     // cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"s "<< endl;     
 
